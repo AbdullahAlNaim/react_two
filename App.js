365c6b2e01bba4f391d149cd6ef5d10cd80a804b
Abdullah Naim <aanaim96@gmail.com>
2023-07-17 16:18:26 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+// import ReactDom from 'react-dom/client'
+// import { act } from 'react-dom/test-utils'
+// import '@testing-library/react'
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
